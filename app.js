bd2806af3207e316b0b85c048298d4f4bc51732c
dinobirdosaurus <mynameispoch@gmail.com>
2023-01-12 08:53:19 +0800

    edit clean up

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function populateBoard(size) {
     let squares = board.querySelectorAll("div");
     squares.forEach((div) => div.remove());
     board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
-    board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 
     let amount = size * size;
     for (let i = 0; i < amount; i++) {
